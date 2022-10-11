7f458c4ebee0a7d8962a02b6ca37911034fbdc06
dlekgus <97431420+dlekgus@users.noreply.github.com>
2022-10-11 13:21:41 +0900

    Add files via upload

---
+++ 3주차/06. BOJ_11651/BOJ_11651_ldh.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+point = []
+
+for i in range(n):
+    [x, y] = map(int, input().split())
+    point.append([x, y])
+
+point.sort(key=lambda x: (x[1], x[0]))
+
+for i in range(n):
+    print(point[i][0], point[i][1])
